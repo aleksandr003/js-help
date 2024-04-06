407afd5722aa41a47e15387413f2c94e8f73da90
aleksandr003 <herasymenko.oleks@gmail.com>
2024-04-05 20:07:04 -0700

    json

---
+++ js/JSON.js
@@ -0,0 +1,18 @@
+//===== stringify
+// const car = {
+//   name: "Toyota",
+//   model: "Camry",
+//   color: "white",
+//   free: true,
+//   price: 32500,
+// };
+
+// const carJSON = JSON.stringify(car);
+// console.log(carJSON);
+
+//===== parse
+// const json =
+//   '{"name":"Toyota","model":"Camry","color":"white","free":true,"price":32500}';
+
+// const car = JSON.parse(json);
+// console.log(car);
